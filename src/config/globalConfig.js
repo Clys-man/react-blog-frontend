8fa58312cb2fb72f9c9f28071025776a5c4bb636
clys-man <clysmanalves@gmail.com>
2020-12-25 17:36:04 -0300

    Changing variables in config

--- src/config/globalConfig.js
+++ src/config/globalConfig.js
@@ -1,9 +1,9 @@
 
 import Axios from 'axios'
 
-export const API_KEY = '';
-export const rootUrl = "https://pa-desafio.herokuapp.com/"
-export const baseUrl = "http://179.199.151.197:3000/"
+export const API_KEY = "" //exemplo: 8d68a5f8daf89dsa86f8asd7f65
+export const rootUrl = "" //exemplo: https://pa-desafio.herokuapp.com/
+export const baseUrl = "" //exemplo: http://localhost:3000/
 
 const basicFetch = async (endpoint) => {
     return (await fetch(`${rootUrl}${endpoint}`)).json();
